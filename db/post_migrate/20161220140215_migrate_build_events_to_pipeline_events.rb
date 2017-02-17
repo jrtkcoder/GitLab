996c17cbf9e0b35ad480da8b4e0ca6db6fcb9071
Lin Jen-Shin <godfat@godfat.org>
2017-02-17 18:35:32 +0800

    Use Gitlab::Database.with_connection_pool

--- db/post_migrate/20161220140215_migrate_build_events_to_pipeline_events.rb
+++ db/post_migrate/20161220140215_migrate_build_events_to_pipeline_events.rb
@@ -1,66 +1,49 @@
 class MigrateBuildEventsToPipelineEvents < ActiveRecord::Migration
   include Gitlab::Database::MigrationHelpers
+  include Gitlab::Database
 
   DOWNTIME = false
 
   def up
-    slack_and_mattermost = spawn <<-SQL.strip_heredoc
-      UPDATE services
-        SET properties = replace(properties,
-                                 'notify_only_broken_builds',
-                                 'notify_only_broken_pipelines')
-          , pipeline_events = #{true_value}
-          , build_events = #{false_value}
-      WHERE type IN ('SlackService', 'MattermostService', 'HipchatService')
-        AND build_events = #{true_value};
-    SQL
+    Gitlab::Database.with_connection_pool(2) do |pool|
+      threads = []
 
-    builds_email = spawn <<-SQL.strip_heredoc
-      UPDATE services
-        SET type = 'PipelinesEmailService'
-          , properties = replace(properties,
-                                 'notify_only_broken_builds',
-                                 'notify_only_broken_pipelines')
-          , pipeline_events = #{true_value}
-          , build_events = #{false_value}
-      WHERE type = 'BuildsEmailService';
-
-      DELETE FROM services WHERE type = 'BuildsEmailService';
-    SQL
-
-    [slack_and_mattermost, builds_email].each(&:join)
-  end
-
-  private
-
-  def spawn(query)
-    Thread.new do
-      with_connection do |connection|
-        query.split(';').each do |statement|
-          next if statement.blank?
-
-          connection.execute(statement)
+      threads << Thread.new do
+        pool.with_connection do |connection|
+          connection.execute(<<-SQL.strip_heredoc)
+            UPDATE services
+              SET properties = replace(properties,
+                                       'notify_only_broken_builds',
+                                       'notify_only_broken_pipelines')
+                , pipeline_events = #{true_value}
+                , build_events = #{false_value}
+            WHERE type IN
+              ('SlackService', 'MattermostService', 'HipchatService')
+              AND build_events = #{true_value};
+          SQL
         end
       end
+
+      threads << Thread.new do
+        pool.with_connection do |connection|
+          connection.execute(<<-SQL.strip_heredoc)
+            UPDATE services
+              SET type = 'PipelinesEmailService'
+                , properties = replace(properties,
+                                       'notify_only_broken_builds',
+                                       'notify_only_broken_pipelines')
+                , pipeline_events = #{true_value}
+                , build_events = #{false_value}
+            WHERE type = 'BuildsEmailService';
+          SQL
+
+          connection.execute(<<-SQL.strip_heredoc)
+            DELETE FROM services WHERE type = 'BuildsEmailService';
+          SQL
+        end
+      end
+
+      threads.each(&:join)
     end
-  end
-
-  def with_connection(&block)
-    pool = ActiveRecord::Base.establish_connection
-    pool.with_connection(&block)
-  ensure
-    pool.disconnect!
-  end
-
-  def quote(value)
-    ActiveRecord::Base.connection.quote(value)
-  end
-
-  def true_value
-    quote(true)
-  end
-
-  def false_value
-    quote(false)
   end
 end
